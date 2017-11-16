ee4ff56677d8de7b60a7bf55701c025372654bf5
CodeFuller <CodeFuller@gmail.com>
2017-11-16 08:37:53 +0300

    Add project for .Net Core Console Application

---
+++ doteNetCore/ConsoleApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+			configurationBuilder.AddJsonFile("AppSettings.json");
+			IConfiguration configuration = configurationBuilder.Build();
+
+			IServiceCollection services = new ServiceCollection();
+			IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+			ILoggerFactory loggerFactory = new LoggerFactory();
+			loggerFactory.AddConsole();
+			ILogger logger = loggerFactory.CreateLogger(String.Empty);
+			logger.LogInformation("Hello :)");
+		}
+	}
+}
