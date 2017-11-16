b96d215eb60824332326e56e491ecb6a8f3dc41c
CodeFuller <CodeFuller@gmail.com>
2017-11-16 20:36:12 +0300

    Add project for ASP.NET WEB API Application

---
+++ dotNet/WebApiApplication/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ dotNet/WebApiApplication/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
