791a0af53efabac13aa679f7af4aec2f389b94b4
CodeFuller <CodeFuller@gmail.com>
2017-11-16 22:06:45 +0300

    Add UnitTests project

---
+++ dotNet/UnitTests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase("ABC", "abc")]
+		public void TestMethod(string inputData, string expectedResult)
+		{
+			var result = inputData.ToLower(CultureInfo.InvariantCulture);
+
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
