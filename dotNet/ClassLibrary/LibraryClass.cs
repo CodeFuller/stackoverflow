a81401863ea45501ad1a38e6a5d20e982e8a9383
CodeFuller <CodeFuller@gmail.com>
2017-11-19 12:58:28 +0300

    Q47320175: "Method not found" exception. Why AppDomain.CurrentDomain.AssemblyResolve doesn't work?

---
+++ dotNet/ClassLibrary/LibraryClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+	public class LibraryClass
+	{
+		public void Test()
+		{
+			Console.WriteLine("Before JsonMediaTypeFormatter().SerializerSettings...");
+
+			//	CF TEMP
+			var assembly = Assembly.LoadFile(@"d:\Dropbox\prog\StackOverflow\dotNet\ConsoleApplication\bin\Debug\System.Net.Http.Formatting.dll");
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+			Do();
+			Console.WriteLine("After JsonMediaTypeFormatter().SerializerSettings!");
+		}
+
+		private void Do()
+		{
+			var settings = new JsonMediaTypeFormatter().SerializerSettings;
+		}
+	}
+}
--- dotNet/ConsoleApplication/Program.cs
+++ dotNet/ConsoleApplication/Program.cs
@@ -1,9 +1,81 @@
-﻿namespace ConsoleApplication
+﻿using System;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication
 {
 	class Program
 	{
+		static void Run()
+		{
+			string output = JsonConvert.SerializeObject(new { Field = "Value" });
+			Console.WriteLine($"Json: {output}");
+		}
+
 		static void Main(string[] args)
 		{
+			try
+			{
+				Console.WriteLine("Started...");
+
+				{
+					var assembly = Assembly.LoadFile(@"d:\Dropbox\prog\StackOverflow\dotNet\ConsoleApplication\bin\Debug\4.5.0\Newtonsoft.Json.dll");
+					var assemblies2 = AppDomain.CurrentDomain.GetAssemblies();
+				}
+
+				{
+					//var assembly = Assembly.LoadFile(@"d:\CodeFuller\_days\2017.11.19\Json\9.0\Newtonsoft.Json.dll");
+					var assemblies2 = AppDomain.CurrentDomain.GetAssemblies();
+				}
+
+				EmbeddedAssembly.Load("ConsoleApplication.Newtonsoft.Json.dll", "Newtonsoft.Json.dll");
+
+				AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
+				Run();
+
+				var libAssembly = Assembly.LoadFile(@"d:\Dropbox\prog\StackOverflow\dotNet\ClassLibrary\bin\Debug\ClassLibrary.dll");
+				var type = libAssembly.GetType("ClassLibrary.LibraryClass");
+				if (type == null)
+				{
+					throw new InvalidOperationException("Class not found");
+				}
+
+				Console.WriteLine("Creating instance...");
+				var obj = Activator.CreateInstance(type);
+				Console.WriteLine("Invoking method...");
+				var method = type.GetMethod("Test");
+
+				var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+				method.Invoke(obj, null);
+				
+				Console.WriteLine("Finished successfully!");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+
+		static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+		{
+			Console.WriteLine($"CurrentDomain_AssemblyResolve({args.Name}) called");
+
+			if (args.Name == "Newtonsoft.Json, Version=8.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed")
+			{
+				var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+				return EmbeddedAssembly.Get(args.Name);
+			}
+
+			if (args.Name == "Newtonsoft.Json, Version=4.5.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed")
+			{
+				var assembly = Assembly.LoadFile(@"d:\Dropbox\prog\StackOverflow\dotNet\ConsoleApplication\bin\Debug\4.5.0\Newtonsoft.Json.dll");
+				var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+				return assembly;
+			}
+			
+			throw new InvalidOperationException($"CurrentDomain_AssemblyResolve() called for unknown assembly: '{args.Name}'");
 		}
 	}
 }
