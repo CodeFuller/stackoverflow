1a868ef9612b8c71dbf6ba926d46a2c81720a54b
CodeFuller <CodeFuller@gmail.com>
2017-11-21 08:24:14 +0300

    Q47405613: Switch-case and out parameter in C#

--- dotNet/ConsoleApplication/Program.cs
+++ dotNet/ConsoleApplication/Program.cs
@@ -1,9 +1,38 @@
 ﻿namespace ConsoleApplication
 {
+class SomeValue
+{
+	public string Foo { get; set; }
+
+	public int Code { get; set; }
+}
+
 	class Program
 	{
+		
+		static SomeValue ExampleFunction(string input)
+		{
+			return new SomeValue
+			{
+				Foo = input,
+				Code = 1,
+			};
+		}
+
 		static void Main(string[] args)
 		{
+			string foo0 = null;
+			string foo1 = null;
+			string foo2 = null;
+			int Type = 1;
+
+			SomeValue val = null;
+			switch (Type)
+			{
+				case 0: val = ExampleFunction(foo0); break;
+				case 1: val = ExampleFunction(foo1); break;
+				case 2: val = ExampleFunction(foo2); break;
+			}
 		}
 	}
 }
