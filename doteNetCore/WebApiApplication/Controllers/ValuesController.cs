05600aa5485b3f3bb07d2de25fde8ae3b398e23d
CodeFuller <CodeFuller@gmail.com>
2017-11-21 18:01:17 +0300

    Q47414920: ModelBinder and Dates in ASP.NET Core 2

--- doteNetCore/WebApiApplication/Controllers/ValuesController.cs
+++ doteNetCore/WebApiApplication/Controllers/ValuesController.cs
@@ -1,23 +1,56 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 
 namespace WebApiApplication.Controllers
 {
+	public class DateTimeModelBinder : IModelBinder
+	{
+		private readonly IModelBinder baseBinder = new SimpleTypeModelBinder(typeof(DateTime));
+
+		public Task BindModelAsync(ModelBindingContext bindingContext)
+		{
+			var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+			if (valueProviderResult != ValueProviderResult.None)
+			{
+				bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
+
+				var valueAsString = valueProviderResult.FirstValue;
+
+				//	valueAsString will have a string value of your date, e.g. '31-12-2017'
+				//	Parse it as you want and build DateTime object
+				var dateTime = DateTime.ParseExact(valueAsString, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+				bindingContext.Result = ModelBindingResult.Success(dateTime);
+
+				return Task.CompletedTask;
+			}
+
+			return baseBinder.BindModelAsync(bindingContext);
+		}
+	}
+
 	[Route("api/[controller]")]
 	public class ValuesController : Controller
 	{
 		// GET api/values
-		[HttpGet]
-		public IEnumerable<string> Get()
-		{
-			return new string[] { "value1", "value2" };
-		}
+		//[HttpGet]
+		//public IEnumerable<string> Get()
+		//{
+		//	return new string[] { "value1", "value2" };
+		//}
 
 		// GET api/values/5
-		[HttpGet("{id}")]
-		public string Get(int id)
+		[HttpGet]
+		public IActionResult Get([FromQuery] [ModelBinder(typeof(DateTimeModelBinder))] DateTime date)
 		{
-			return "value";
+			return null;
 		}
 
 		// POST api/values
