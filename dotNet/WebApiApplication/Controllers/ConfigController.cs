6d6f809abe3b6ce0f4879cbfd3b42789a1851322
CodeFuller <CodeFuller@gmail.com>
2017-11-21 07:26:41 +0300

    Q47404968: Routing issue in self hosted webapi

---
+++ dotNet/WebApiApplication/Controllers/ConfigController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiApplication.Controllers
+{
+	//[RoutePrefix("api")]
+	public class ConfigController : ApiController
+	{
+		[HttpGet, Route("api/{id}/flows/config")]
+		public string SayHello([FromUri] string id)
+		{
+			return "Hello " + id;
+		}
+	}
+}
