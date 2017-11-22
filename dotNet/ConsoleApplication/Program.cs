7d1dc22ddb4a89fed83a87a240b5b660519a1b25
CodeFuller <CodeFuller@gmail.com>
2017-11-22 07:55:38 +0300

    Q47426937: Split a array into multiple array

--- dotNet/ConsoleApplication/Program.cs
+++ dotNet/ConsoleApplication/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace ConsoleApplication
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication
 {
 	class Program
 	{
+		public static IEnumerable<List<T>> SplitArray<T>(T[] arr, int splitsNumber)
+		{
+			var list = arr.ToList();
+			int size = list.Count / splitsNumber;
+			int pos = 0;
+			for (int i = 0; i + 1 < splitsNumber; ++i, pos += size)
+			{
+				yield return list.GetRange(pos, size);
+			}
+
+			yield return list.GetRange(pos, list.Count - pos);
+		}
+
 		static void Main(string[] args)
 		{
+			var arr = new int[] {1, 2, 3, 4, 5, 6, 7};
+			var res = SplitArray(arr, 3).ToList();
 		}
 	}
 }
