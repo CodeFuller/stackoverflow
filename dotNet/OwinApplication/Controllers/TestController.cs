aaf2cd60af7976c3fdde612261b6ca47ad385b16
CodeFuller <CodeFuller@gmail.com>
2017-11-22 08:01:34 +0300

    Add project for OWIN Self-Hosting Application

---
+++ dotNet/OwinApplication/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OwinApplication.Controllers
+{
+	[RoutePrefix("api/test")]
+	public class TestController : ApiController
+	{
+		[Route]
+		[HttpGet]
+		public string Test()
+		{
+			return "Hello";
+		}
+	}
+}
---
+++ dotNet/OwinApplication/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using Microsoft.Owin;
+using Owin;
+
+namespace OwinApplication
+{
+	/// <summary>
+	/// Startup configuration logic for REST API
+	/// </summary>
+	internal class Startup
+	{
+		// Suppressing this message, which is triggered because HttpConfiguration is IDispoable.  But if
+		// we dispose of this configuration on startup, our program won't work!
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+		public static void Configuration(IAppBuilder app)
+		{
+			// Configure Web API for self-host. 
+			HttpConfiguration config = new HttpConfiguration();
+
+			// Enable specification of attributes like [RoutePrefix], [Route], etc. in controller classes
+			config.MapHttpAttributeRoutes();
+
+			// Configure JSON response from REST API
+			config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+			app.UseWebApi(config);
+
+			//var url = HttpContext.Current.Request.Url.AbsoluteUri;
+			//string loginPath = "/auth/login";
+			//string areaName = string.Empty;
+			//if (url.ToLower().Contains("/su/"))
+			//{
+			//	areaName = "SU";
+			//	loginPath = "/su/auth/login";
+			//}
+			//if (url.ToLower().Contains("/app/"))
+			//{
+			//	areaName = "APP";
+			//	loginPath = "/app/auth/login";
+			//}
+			//app.UseCookieAuthentication(new CookieAuthenticationOptions
+			//{
+			//	AuthenticationType = "ApplicationCookie" + areaName,
+			//	LoginPath = new PathString(loginPath)
+			//});
+		}
+	}
+}
