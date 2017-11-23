e4c319a3cb24825f7d7b5d8f65569f5cd382cb9f
CodeFuller <CodeFuller@gmail.com>
2017-11-23 08:08:45 +0300

    Q47447727: convert float to string preserving decimals

--- dotNet/ConsoleApplication/Program.cs
+++ dotNet/ConsoleApplication/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace ConsoleApplication
+﻿using System;
+
+namespace ConsoleApplication
 {
+public static class FloatExtensions
+{
+	public static string Format(this float f, int n)
+	{
+		return f.ToString($"0.{new String('0', n)}");
+	}
+}
+
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			float rmt = 100;
+			int n = 2;
+
+			var str = rmt.Format(n);
 		}
 	}
 }
