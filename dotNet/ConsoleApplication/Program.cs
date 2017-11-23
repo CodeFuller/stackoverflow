01727fc72e64b274a23a54c78d30f6e0ea6ddce4
CodeFuller <CodeFuller@gmail.com>
2017-11-23 08:28:01 +0300

    Add solution without string interpolation

--- dotNet/ConsoleApplication/Program.cs
+++ dotNet/ConsoleApplication/Program.cs
@@ -6,7 +6,8 @@ public static class FloatExtensions
 {
 	public static string Format(this float f, int n)
 	{
-		return f.ToString($"0.{new String('0', n)}");
+		//return f.ToString($"0.{new String('0', n)}");
+		return f.ToString("0." + new String('0', n));
 	}
 }
 
