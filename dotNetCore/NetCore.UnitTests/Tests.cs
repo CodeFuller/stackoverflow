a6776a21764276d4e808d805f1af8b0e8ae8cf04
CodeFuller <CodeFuller@gmail.com>
2017-11-24 14:16:25 +0300

    Add Unit Tests project for .Net Core

---
+++ dotNetCore/NetCore.UnitTests/Tests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetCore.UnitTests
+{
+	[TestClass]
+	public class Tests
+	{
+		[TestMethod]
+		public void SaveVisitorInfoTest()
+		{
+			Assert.IsTrue(true);
+		}
+	}
+}
