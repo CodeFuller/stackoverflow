8d39575c7bc5443daea17d8445712eacbd6684ee
CodeFuller <CodeFuller@gmail.com>
2017-11-24 08:07:38 +0300

    Q47466488: Check time taken by a thread inside Parallel.Foreach and exit if it is taking more time

--- dotNet/ConsoleApplication/Program.cs
+++ dotNet/ConsoleApplication/Program.cs
@@ -1,9 +1,47 @@
-﻿namespace ConsoleApplication
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			List<string> lstFiles = new List<string>()
+			{
+				"1",
+				"2"
+			};
+
+			var tasks = lstFiles.Select(f => Task.Run(() => ActionOnFile(f))).ToArray();
+			Task.WaitAll(tasks, TimeSpan.FromMinutes(2));
+
+			Parallel.ForEach(lstFiles, file =>
+			{
+				var cts = new CancellationTokenSource(TimeSpan.FromMinutes(2));
+				var task = Task.Run(() => ActionOnFile(file));
+				try
+				{
+					task.Wait(cts.Token);
+				}
+				catch (OperationCanceledException)
+				{
+				}
+			});
+
+			//Parallel.ForEach(lstFiles, file =>
+			//{
+			//	// Doing some operation on file
+			//	// Skip file and move to next if it is taking too long
+			//});
+		}
+
+		static void ActionOnFile(string file)
+		{
+			Thread.Sleep(3000);
 		}
 	}
 }
