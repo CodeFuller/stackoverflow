a73acfd2870a39d417492e970b00eeae1fbd2de3
CodeFuller <CodeFuller@gmail.com>
2017-11-28 09:25:42 +0300

    Q47501194: Binding logging configuration from appsettings to POCO Asp.Net Core 2

--- dotNetCore/NetCore.ConsoleApplication/Program.cs
+++ dotNetCore/NetCore.ConsoleApplication/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using NetCore.ConsoleApplication.Dal;
 
 namespace NetCore.ConsoleApplication
 {
+	public class LoggingLevel
+	{
+		public LogLevel Default { get; set; }
+	}
+
+	public class LoggerSettings
+	{
+		public bool IncludeScopes { get; set; }
+
+		public LoggingLevel LogLevel { get; set; }
+	}
+
+	public class ApplicationConfigurationSettings
+	{
+		public LoggerSettings Logging { get; set; }
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
@@ -14,7 +33,11 @@ namespace NetCore.ConsoleApplication
 			configurationBuilder.AddJsonFile("AppSettings.json");
 			IConfiguration configuration = configurationBuilder.Build();
 
+			var settings = new ApplicationConfigurationSettings();
+			configuration.GetSection("ApplicationConfiguration").Bind(settings);
+
 			IServiceCollection services = new ServiceCollection();
+			services.Configure<ApplicationConfigurationSettings>(configuration.GetSection("ApplicationConfiguration"));
 			IServiceProvider serviceProvider = services.BuildServiceProvider();
 
 			ILoggerFactory loggerFactory = new LoggerFactory();
