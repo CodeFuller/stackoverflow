565f91a1c2972667fe4ad109a259d382c145b257
CodeFuller <CodeFuller@gmail.com>
2017-11-29 13:34:46 +0300

    Add project for WCF Application

---
+++ WcfServiceApplication/SomeService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WcfServiceApplication
+{
+	public class SomeService : ISomeService
+	{
+		public string GetData(int value)
+		{
+			return string.Format("You entered: {0}", value);
+		}
+
+		public CompositeType GetDataUsingDataContract(CompositeType composite)
+		{
+			if (composite == null)
+			{
+				throw new ArgumentNullException("composite");
+			}
+			if (composite.BoolValue)
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+	}
+}
