4b54adbfe483e156107aeb23ca369736b0e87f72
CodeFuller <CodeFuller@gmail.com>
2017-11-30 09:07:28 +0300

    Q47565008: Trouble converting Serilog Configuration code line to json configuration

--- dotNetCore/NetCore.ConsoleApplication/Program.cs
+++ dotNetCore/NetCore.ConsoleApplication/Program.cs
@@ -3,26 +3,47 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NetCore.ConsoleApplication.Dal;
+using Serilog;
+using Serilog.Events;
 
 namespace NetCore.ConsoleApplication
 {
+public class SubLoggerConfiguration
+{
+	public LogEventLevel Level { get; set; }
+
+	public string PathFormat { get; set; }
+}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-			configurationBuilder.AddJsonFile("AppSettings.json");
-			IConfiguration configuration = configurationBuilder.Build();
-
 			IServiceCollection services = new ServiceCollection();
 			IServiceProvider serviceProvider = services.BuildServiceProvider();
 
-			ILoggerFactory loggerFactory = new LoggerFactory();
-			loggerFactory.AddConsole();
-			ILogger logger = loggerFactory.CreateLogger(String.Empty);
-			logger.LogInformation("Hello :)");
+			//ILoggerFactory loggerFactory = new LoggerFactory();
+			//loggerFactory.AddConsole();
+			//ILogger logger = loggerFactory.CreateLogger(String.Empty);
+			//logger.LogInformation("Hello :)");
 
-			new MusicLibraryRepository().Test();
+			IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+			configurationBuilder.AddJsonFile("AppSettings.json");
+			IConfiguration configuration = configurationBuilder.Build();
+			SubLoggerConfiguration subLoggerConfiguration = new SubLoggerConfiguration();
+			configuration.GetSection("Serilog:SubLogger").Bind(subLoggerConfiguration);
+			
+			//var log = new LoggerConfiguration()
+			//	.ReadFrom.Configuration(configuration)
+			//	.CreateLogger();
+
+			var Logger = new LoggerConfiguration()
+				.MinimumLevel.Information()
+				.WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == subLoggerConfiguration.Level).WriteTo.RollingFile(subLoggerConfiguration.PathFormat));
+
+			var log = Logger.CreateLogger();
+			log.Information("Information message");
+			log.Warning("Warning message");
 		}
 	}
 }
