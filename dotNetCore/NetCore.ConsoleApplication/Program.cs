4f831ef81a60242e14c821d5fb0aac0372d2438e
CodeFuller <CodeFuller@gmail.com>
2017-12-05 09:41:16 +0300

    Q47564501: ASP.Net JSON configuration file transforms with arrays

--- dotNetCore/NetCore.ConsoleApplication/Program.cs
+++ dotNetCore/NetCore.ConsoleApplication/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -6,23 +9,81 @@ using NetCore.ConsoleApplication.Dal;
 
 namespace NetCore.ConsoleApplication
 {
+	public static class ConfigurationRootExtensions
+	{
+		private static readonly Regex ArrayKeyRegex = new Regex("^(.+):\\d+$", RegexOptions.Compiled);
+
+		public static IConfigurationRoot FixOverridenArrays(this IConfigurationRoot configurationRoot)
+		{
+			HashSet<string> knownArrayKeys = new HashSet<string>();
+
+			foreach (IConfigurationProvider provider in configurationRoot.Providers.Reverse())
+			{
+				HashSet<string> currProviderArrayKeys = new HashSet<string>();
+
+				foreach (var key in GetProviderKeys(provider, null).Reverse())
+				{
+					//	Is this an array value?
+					var match = ArrayKeyRegex.Match(key);
+					if (match.Success)
+					{
+						var arrayKey = match.Groups[1].Value;
+						//	Some provider overrides this array.
+						//	Suppressing the value.
+						if (knownArrayKeys.Contains(arrayKey))
+						{
+							provider.Set(key, null);
+						}
+						else
+						{
+							currProviderArrayKeys.Add(arrayKey);
+						}
+					}
+				}
+
+				foreach (var key in currProviderArrayKeys)
+				{
+					knownArrayKeys.Add(key);
+				}
+			}
+
+			return configurationRoot;
+		}
+
+		private static IEnumerable<string> GetProviderKeys(IConfigurationProvider provider,
+			string parentPath)
+		{
+			var prefix = parentPath == null
+					? string.Empty
+					: parentPath + ConfigurationPath.KeyDelimiter;
+
+			List<string> keys = new List<string>();
+			var childKeys = provider.GetChildKeys(Enumerable.Empty<string>(), parentPath)
+				.Distinct()
+				.Select(k => prefix + k).ToList();
+			keys.AddRange(childKeys);
+			foreach (var key in childKeys)
+			{
+				keys.AddRange(GetProviderKeys(provider, key));
+			}
+
+			return keys;
+		}
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
 			IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-			configurationBuilder.AddJsonFile("AppSettings.json");
-			IConfiguration configuration = configurationBuilder.Build();
+			configurationBuilder.AddJsonFile("AppSettings.json").AddJsonFile("appsettings.dev.json");
+			var configuration = configurationBuilder.Build();
+			configuration.FixOverridenArrays();
 
-			IServiceCollection services = new ServiceCollection();
-			IServiceProvider serviceProvider = services.BuildServiceProvider();
-
-			ILoggerFactory loggerFactory = new LoggerFactory();
-			loggerFactory.AddConsole();
-			ILogger logger = loggerFactory.CreateLogger(String.Empty);
-			logger.LogInformation("Hello :)");
-
-			new MusicLibraryRepository().Test();
+			//IConfigurationProvider
+			var valuesSection = configuration.GetSection("Values");
+			var vvv = configuration["Values:Test:1"];
+			//var test = valuesSection["Test"];
 		}
 	}
 }
