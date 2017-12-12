e99b7ed9ed93cdd1204b526d307d2e18dcb6f281
CodeFuller <CodeFuller@gmail.com>
2017-12-12 08:50:59 +0300

    Q47757621: finding Index of particular Action In Queue

--- dotNet/ConsoleApplication/Program.cs
+++ dotNet/ConsoleApplication/Program.cs
@@ -1,12 +1,37 @@
-﻿using ConsoleApplication.Dal;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConsoleApplication.Dal;
 
 namespace ConsoleApplication
 {
+	public class QueueItem
+	{
+		public string SessionName { get; }
+
+		public Func<string> Func => () => SessionName;
+
+		public QueueItem(string sessionName)
+		{
+			SessionName = sessionName;
+		}
+	}
+
 	class Program
 	{
+		public static Queue<QueueItem> myQ = new Queue<QueueItem>();
+
 		static void Main(string[] args)
 		{
-			new MusicLibraryRepository().Test();
+			myQ.Enqueue(new QueueItem("First"));
+			myQ.Enqueue(new QueueItem("Second"));
+			myQ.Enqueue(new QueueItem("Thrid"));
+			var queueList = myQ.ToList();
+			int index = queueList.FindIndex(x => x.SessionName == "First");
+			var result = queueList[index].Func();
+
+			Console.WriteLine(index);
+			Console.ReadLine();
 		}
 	}
 }
