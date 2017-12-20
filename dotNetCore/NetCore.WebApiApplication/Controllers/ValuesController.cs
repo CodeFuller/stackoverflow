3410e398cd156627999c91f9f1eb5fc02fd5f916
CodeFuller <CodeFuller@gmail.com>
2017-12-20 09:08:55 +0300

    Q47898410: ASP.NET Core ExceptionFilterAttribute access Controller

--- dotNetCore/NetCore.WebApiApplication/Controllers/ValuesController.cs
+++ dotNetCore/NetCore.WebApiApplication/Controllers/ValuesController.cs
@@ -1,8 +1,64 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace NetCore.WebApiApplication.Controllers
 {
+	/// <summary>
+	/// Error handler for webapi calls. Adds tracking to base controller.
+	/// </summary>
+	public class ApiExceptionAttribute : ExceptionFilterAttribute
+	{
+		public override void OnException(ExceptionContext context)
+		{
+			var items = context.HttpContext.Items;
+			if (items.ContainsKey("Tracking"))
+			{
+				Tracking tracking = (Tracking)items["Tracking"];
+				tracking.Exception(context.Exception, true);
+			}
+
+			base.OnException(context);
+		}
+	}
+
+	public class ApiTrackingAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			var tracking = new Tracking();
+			//...add info to tracking
+
+			context.HttpContext.Items.Add("Tracking", tracking);
+		}
+
+		public override void OnActionExecuted(ActionExecutedContext context)
+		{
+			var items = context.HttpContext.Items;
+			if (items.ContainsKey("Tracking"))
+			{
+				Tracking tracking = (Tracking) items["Tracking"];
+				tracking.Save();
+			}
+		}
+	}
+
+	public class Tracking
+	{
+		public void Save()
+		{
+			//throw new System.NotImplementedException();
+		}
+
+		public void Exception(Exception contextException, bool b)
+		{
+			//throw new NotImplementedException();
+		}
+	}
+
+	[ApiTracking]
+	[ApiException]
 	[Route("api/[controller]")]
 	public class ValuesController : Controller
 	{
@@ -10,6 +66,7 @@ namespace NetCore.WebApiApplication.Controllers
 		[HttpGet]
 		public IEnumerable<string> Get()
 		{
+			throw new InvalidOperationException();
 			return new string[] { "value1", "value2" };
 		}
 
