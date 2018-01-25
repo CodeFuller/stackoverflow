4a66f4a544a8eb21a1d123e2240f70ba5bf17e0a
CodeFuller <CodeFuller@gmail.com>
2018-01-25 21:58:10 +0300

    Q48431952: MongoDB using a C# driver and time interval LINQ

--- dotNet/MongoDbClient/Program.cs
+++ dotNet/MongoDbClient/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using MongoDB.Driver.Core.Events;
 
@@ -7,6 +9,36 @@ namespace MongoDbClient
 {
 	public class SomeDocument
 	{
+		public ObjectId Id { get; set; }
+
+		[BsonElement("time")]
+		public DateTime Time { get; set; }
+	}
+
+	public class AggregatedDateTime
+	{
+		[BsonElement("year")]
+		public int Year { get; set; }
+
+		[BsonElement("month")]
+		public int Month { get; set; }
+
+		[BsonElement("day")]
+		public int Day { get; set; }
+
+		[BsonElement("hour")]
+		public int Hour { get; set; }
+
+		[BsonElement("minute")]
+		public int Minute { get; set; }
+	}
+
+	public class DateTimeStats
+	{
+		public AggregatedDateTime Id { get; set; }
+
+		[BsonElement("count")]
+		public int Count { get; set; }
 	}
 
 	class Program
@@ -26,8 +58,79 @@ namespace MongoDbClient
 			});
 
 			var database = mongoClient.GetDatabase("testDB");
-			var collection = database.GetCollection<SomeDocument>("test");
-			collection.InsertOne(new SomeDocument());
+			var collection = database.GetCollection<SomeDocument>("foo");
+
+			var project = new BsonDocument
+			{
+				{
+					"$project",
+					new BsonDocument
+					{
+						{
+							"date",
+							new BsonDocument
+							{
+								{ "year", new BsonDocument { {"$year", "$time"} } },
+								{ "month", new BsonDocument { {"$month", "$time" } } },
+								{ "day", new BsonDocument { { "$dayOfMonth", "$time" } } },
+								{ "hour", new BsonDocument { { "$hour", "$time" } } },
+								{ "minute",
+									new BsonDocument
+									{
+										{
+											"$subtract",
+											new BsonArray
+											{
+												new BsonDocument { { "$minute", "$time"} },
+												new BsonDocument
+												{
+													{
+														"$mod", new BsonArray
+														{
+															new BsonDocument { { "$minute", "$time"} },
+															10
+														}
+													}
+												},
+											}
+										}
+									}
+								},
+							}
+						}
+					}
+				}
+			};
+
+			var group = new BsonDocument
+			{
+				{ "$group",
+					new BsonDocument
+					{
+						{"_id", "$date"},
+						{"count", new BsonDocument("$sum", 1)}
+					}
+				}
+			};
+
+			var pipeline = new[] { project, group };
+			var results = collection.Aggregate<DateTimeStats>(pipeline).ToList();
+
+			var result2 = collection.AsQueryable().Select(x => new
+			{
+				Date = new
+				{
+					Year = x.Time.Year,
+					Month = x.Time.Month,
+					Day = x.Time.Day,
+					Hour = x.Time.Hour,
+					Minute = x.Time.Minute - x.Time.Minute % 10,
+				}
+			}).GroupBy(x => x.Date).Select(g => new
+			{
+				Date = g.Key,
+				Count = g.Count(),
+			}).ToList();
 		}
 	}
 }
