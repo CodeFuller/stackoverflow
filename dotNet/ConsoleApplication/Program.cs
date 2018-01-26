a5635c9eae30aead69d3ce74666e06039083fa7b
CodeFuller <CodeFuller@gmail.com>
2018-01-26 09:35:23 +0300

    Q48446422: How to mock a decorated class in an integration test

--- dotNet/ConsoleApplication/Program.cs
+++ dotNet/ConsoleApplication/Program.cs
@@ -1,12 +1,60 @@
-﻿using ConsoleApplication.Dal;
+﻿using System.Collections.Generic;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using ConsoleApplication.Dal;
 
 namespace ConsoleApplication
 {
+	public interface IRepository
+	{
+	}
+
+	public class ComputedColumns : IRepository
+	{
+	}
+
+	public class DataRepository : IRepository
+	{
+	}
+
+	public interface ICacheService<TKey, TValue>
+	{
+	}
+
+	public class MyData
+	{
+	}
+
+	public class MyDataCache : IRepository, ICacheService<int, List<MyData>>
+	{
+	}
+
+public class Installer<TDataRepository> : IWindsorInstaller where TDataRepository : IRepository
+{
+	public void Install(IWindsorContainer container, IConfigurationStore store)
+	{
+		container.Register(Component.For<IRepository, ICacheService<int, List<MyData>>>().ImplementedBy<MyDataCache>());
+		container.Register(Component.For<IRepository>().ImplementedBy<ComputedColumns>());
+		container.Register(Component.For<IRepository>().ImplementedBy<TDataRepository>());
+	}
+}
+
+	public class MockedRepository : IRepository
+	{
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			new MusicLibraryRepository().Test();
+			var container = new WindsorContainer();
+
+			//	In production code
+			container.Install(new Installer<DataRepository>());
+
+			//	In integration test
+			container.Install(new Installer<MockedRepository>());
 		}
 	}
 }
