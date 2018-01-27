85842a67bf647681a67bb260a8cb58178c618d10
CodeFuller <CodeFuller@gmail.com>
2018-01-27 10:07:09 +0300

    Q48470090: serilog format SourceContext for showing only assembly name

--- dotNetCore/NetCore.ConsoleApplication/Program.cs
+++ dotNetCore/NetCore.ConsoleApplication/Program.cs
@@ -2,34 +2,84 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NetCore.ConsoleApplication.Dal;
+using Serilog;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace NetCore.ConsoleApplication
 {
-	public class SomeSettings
+	public class LoggerEx<T> : ILogger<T>
 	{
-		public string SomeValue { get; set; }
+		private readonly ILogger _logger;
+
+		public LoggerEx(ILoggerFactory factory)
+		{
+			if (factory == null)
+			{
+				throw new ArgumentNullException(nameof(factory));
+			}
+
+			_logger = factory.CreateLogger(typeof(T).Assembly.GetName().Name);
+		}
+
+		IDisposable ILogger.BeginScope<TState>(TState state)
+		{
+			return _logger.BeginScope(state);
+		}
+
+		bool ILogger.IsEnabled(LogLevel logLevel)
+		{
+			return _logger.IsEnabled(logLevel);
+		}
+
+		void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+		{
+			_logger.Log(logLevel, eventId, state, exception, formatter);
+		}
+	}
+
+	public interface ISomeInterface
+	{
+		void SomeMethod();
+	}
+
+	public class SomeClass : ISomeInterface
+	{
+		private readonly ILogger<SomeClass> logger;
+
+		public SomeClass(ILogger<SomeClass> logger)
+		{
+			this.logger = logger;
+		}
+
+		public void SomeMethod()
+		{
+			logger.LogInformation("Hello!");
+		}
 	}
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-			configurationBuilder.AddJsonFile("AppSettings.json");
-			IConfiguration configuration = configurationBuilder.Build();
+			var appConfiguration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", optional: false)
+				.Build();
 
-			IServiceCollection services = new ServiceCollection();
-			IServiceProvider serviceProvider = services.BuildServiceProvider();
+			var configuration = new LoggerConfiguration();
+			configuration.ReadFrom.Configuration(appConfiguration);
+			Log.Logger = configuration.CreateLogger();
 
-			ILoggerFactory loggerFactory = new LoggerFactory();
-			loggerFactory.AddConsole();
-			ILogger logger = loggerFactory.CreateLogger(String.Empty);
-			logger.LogInformation("Hello :)");
+			var services = new ServiceCollection();
+			services.AddTransient<ISomeInterface, SomeClass>();
+			services.AddLogging(lb =>
+			{
+				lb.AddSerilog();
+			});
+			services.AddSingleton(typeof(ILogger<>), typeof(LoggerEx<>));
 
-			SomeSettings settings = configuration.GetSection("SomeSettings").Get<SomeSettings>();
-
-			new MusicLibraryRepository().Test();
+			var serviceProvider = services.BuildServiceProvider();
+			var obj = serviceProvider.GetService<ISomeInterface>();
+			obj.SomeMethod();
 		}
 	}
 }
