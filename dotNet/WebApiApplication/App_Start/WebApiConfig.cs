7f4f480d7c97d7a6fef53ec74298954d9d62a71a
CodeFuller <CodeFuller@gmail.com>
2018-02-07 08:05:35 +0300

    Q48644525: A simple post request wont route to a web api control

--- dotNet/WebApiApplication/App_Start/WebApiConfig.cs
+++ dotNet/WebApiApplication/App_Start/WebApiConfig.cs
@@ -8,14 +8,30 @@ namespace WebApiApplication
 		{
 			// Web API configuration and services
 
+			config.Routes.MapHttpRoute(
+				name: "ActionApi",
+				routeTemplate: "api/{controller}/{action}");
 			// Web API routes
-			config.MapHttpAttributeRoutes();
 
 			config.Routes.MapHttpRoute(
 				name: "DefaultApi",
 				routeTemplate: "api/{controller}/{id}",
 				defaults: new { id = RouteParameter.Optional }
 			);
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultAdpi",
+				routeTemplate: "api/ms/{controller}/{action}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			config.Routes.MapHttpRoute(
+				name: "MSApi",
+				routeTemplate: "api/{controller}/ms/{action}",
+				defaults: new { controller = "GPI" }
+			);
+
+			config.MapHttpAttributeRoutes();
 		}
 	}
 }
---
+++ dotNet/WebApiApplication/Controllers/GoldenController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebApiApplication.Controllers
+{
+	public class SomeClass
+	{
+		public string T { get; set; }
+	}
+
+	public class GoldenController : ApiController
+	{
+		[HttpPost]
+		public IHttpActionResult Payout([FromBody] string t)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
