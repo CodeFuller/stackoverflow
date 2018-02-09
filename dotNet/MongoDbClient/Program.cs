b008a1eecf17a48024bb8271616f72a1d2e3e777
CodeFuller <CodeFuller@gmail.com>
2018-02-09 08:10:47 +0300

    Q48696487: MongoDB - Update field or Add field if not present on BsonDocument C#

--- dotNet/MongoDbClient/Program.cs
+++ dotNet/MongoDbClient/Program.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using MongoDB.Driver.Core.Events;
 
 namespace MongoDbClient
 {
-	public class SomeDocument
+	public class SomeMongoDocument
 	{
+		public ObjectId _id { get; set; }
+
+		public string FileName { get; set; }
+
+		public BsonDocument Metadata { get; set; }
 	}
 
 	class Program
 	{
+		private IMongoCollection<SomeMongoDocument> MongoCollection { get; set; }
+
 		static void Main(string[] args)
 		{
+			Program app = new Program();
+			app.Run();
+		}
+
+		private void Run()
+		{
+			var filename = "SomeFilename";
+			var metadata = "{ \"field1\": \"NewValue1\", \"field2\": \"value2\" }";
+
 			var mongoClient = new MongoClient(new MongoClientSettings
 			{
 				Server = new MongoServerAddress("localhost", 27017),
@@ -26,8 +43,23 @@ namespace MongoDbClient
 			});
 
 			var database = mongoClient.GetDatabase("testDB");
-			var collection = database.GetCollection<SomeDocument>("test");
-			collection.InsertOne(new SomeDocument());
+			MongoCollection = database.GetCollection<SomeMongoDocument>("metadata");
+
+			var filter = Builders<SomeMongoDocument>.Filter.Eq(e => e.FileName, filename);
+
+			BsonDocument document = BsonSerializer.Deserialize<BsonDocument>(metadata);
+
+			UpdateDefinition<SomeMongoDocument> update = null;
+			foreach (BsonElement element in document)
+			{
+				update = update?.Set(e => e.Metadata[element.Name], element.Value) ??
+							Builders<SomeMongoDocument>.Update.Set(e => e.Metadata[element.Name], element.Value);
+			}
+
+			if (update != null)
+			{
+				this.MongoCollection.UpdateOne(filter, update);
+			}
 		}
 	}
 }
