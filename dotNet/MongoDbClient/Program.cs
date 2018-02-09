eab030dd08df1214fd27a7d2f6cadc6b7dd48fe8
CodeFuller <CodeFuller@gmail.com>
2018-02-09 09:02:22 +0300

    Q48689561: Custom serializer that processes both nullable and not nullable types for MongoDb

--- dotNet/MongoDbClient/Program.cs
+++ dotNet/MongoDbClient/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using MongoDB.Driver.Core.Events;
 
 namespace MongoDbClient
 {
+	internal class MyCustomDateTimeSerializer<TDateTime> : IBsonSerializer
+	{
+		static MyCustomDateTimeSerializer()
+		{
+			if (typeof(TDateTime) != typeof(DateTime) && typeof(TDateTime) != typeof(DateTime?))
+			{
+				throw new InvalidOperationException($"MyCustomDateTimeSerializer could be used only with {nameof(DateTime)} or {nameof(Nullable<DateTime>)}");
+			}
+		}
+		
+		public object Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+		{
+			var data = context.Reader.ReadString();
+
+			// Deserialization logic
+			if (ValueType == typeof(DateTime?))
+			{
+				return null;
+			}
+
+			return new DateTime(2018, 2, 9);
+		}
+
+		public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
+		{
+			// Serialization logic
+			throw  new NotImplementedException();
+		}
+
+		public Type ValueType => typeof(TDateTime);
+	}
+
 	public class SomeDocument
 	{
+		public ObjectId _id { get; set; }
+
+		[BsonSerializer(typeof(MyCustomDateTimeSerializer<DateTime>))]
+		//[BsonSerializer(typeof(MyCustomDateTimeSerializer1))]
+		public DateTime Date1 { get; set; }
+
+		//[BsonSerializer(typeof(MyCustomDateTimeSerializer2))]
+		[BsonSerializer(typeof(MyCustomDateTimeSerializer<DateTime?>))]
+		public DateTime? Date2 { get; set; }
 	}
 
 	class Program
@@ -27,7 +70,7 @@ namespace MongoDbClient
 
 			var database = mongoClient.GetDatabase("testDB");
 			var collection = database.GetCollection<SomeDocument>("test");
-			collection.InsertOne(new SomeDocument());
+			var docs = collection.AsQueryable().ToList();
 		}
 	}
 }
