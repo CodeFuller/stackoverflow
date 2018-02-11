3ccdd27ac95e72ebadc6370f449c3dd25144a57e
CodeFuller <CodeFuller@gmail.com>
2018-02-11 18:49:52 +0300

    Q48732302: How to get the contents a particular cell using excel data reader in c#?

--- dotNet/ConsoleApplication/Program.cs
+++ dotNet/ConsoleApplication/Program.cs
@@ -1,4 +1,7 @@
-﻿using ConsoleApplication.Dal;
+﻿using System;
+using System.Data;
+using System.IO;
+using ExcelDataReader;
 
 namespace ConsoleApplication
 {
@@ -6,7 +9,40 @@ namespace ConsoleApplication
 	{
 		static void Main(string[] args)
 		{
-			new MusicLibraryRepository().Test();
+			FileStream stream = File.Open(@"C:\Users\Bumba\Downloads\Sample-Sales-Data.xlsx", FileMode.Open, FileAccess.Read);
+			IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+			DataSet result = excelReader.AsDataSet(new ExcelDataSetConfiguration()
+			{
+				ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+				{
+					UseHeaderRow = true
+				}
+			});
+
+			foreach (DataTable table in result.Tables)
+			{
+				foreach (DataRow row in table.Rows)
+				{
+					var salesRepName = (string)row["Sales_Rep_Name"];
+					if (String.Equals(salesRepName, "Janet"))
+					{
+						var year = (int)(double)row["Year"];
+						Console.WriteLine($"Janet's year is {year}");
+					}
+				}
+			}
+
+			foreach (DataTable table in result.Tables)
+			{
+				foreach (DataRow row in table.Select("Sales_Rep_Name = 'Janet'"))
+				{
+					var year = (int)(double)row["Year"];
+					Console.WriteLine($"Janet's year is {year}");
+				}
+			}
+
+			excelReader.Close();
 		}
 	}
 }
