c1ae88b7330625e08071666c4e54681aed87408b
CodeFuller <CodeFuller@gmail.com>
2018-02-28 19:21:10 +0300

    Q48790497: How to convert Expression into CSharpCompilation or CSharpSyntaxTree?

---
+++ dotNet/ConsoleApplication/ExpressionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using AgileObjects.ReadableExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace ConsoleApplication
+{
+	public static class ExpressionExtensions
+	{
+		public static SyntaxTree ToSyntaxTree(this Expression expression)
+		{
+			var expressionCode = expression.ToReadableString();
+			return CSharpSyntaxTree.ParseText(expressionCode);
+		}
+	}
+}
--- dotNet/ConsoleApplication/Program.cs
+++ dotNet/ConsoleApplication/Program.cs
@@ -1,12 +1,31 @@
-﻿using ConsoleApplication.Dal;
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.CodeAnalysis;
 
 namespace ConsoleApplication
 {
+	class HasIndexers
+	{
+		public object this[string s] => null;
+
+		public object this[int i] => null;
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			new MusicLibraryRepository().Test();
+			var p = Expression.Parameter(typeof(int), "p");
+			Expression assignment = Expression.Assign(p, Expression.Constant(1));
+			Expression addAssignment = Expression.AddAssign(p, Expression.Constant(5));
+			BlockExpression addAssignmentBlock = Expression.Block(
+				new ParameterExpression[] { p },
+				assignment, addAssignment);
+
+			SyntaxTree tree1 = addAssignmentBlock.ToSyntaxTree();
+
+			Expression<Func<bool>> expr = () => new HasIndexers()[3] == new HasIndexers()["three"];
+			SyntaxTree tree2 = expr.ToSyntaxTree();
 		}
 	}
 }
