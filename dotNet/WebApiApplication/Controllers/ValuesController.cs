9d94814b3b3465444feb4e43e4e2b2713c02e774
CodeFuller <CodeFuller@gmail.com>
2018-03-13 14:55:19 +0300

    Q49094779: How to debug an external process (poweshell) when no error message or exception

--- dotNet/WebApiApplication/Controllers/ValuesController.cs
+++ dotNet/WebApiApplication/Controllers/ValuesController.cs
@@ -1,35 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Web.Http;
+using Microsoft.Win32.TaskScheduler;
 
 namespace WebApiApplication.Controllers
 {
 	public class ValuesController : ApiController
 	{
 		// GET api/values
-		public IEnumerable<string> Get()
+		public string Get()
 		{
-			return new string[] { "value1", "value2" };
-		}
+			string powerShellScript = @"d:\Dropbox\prog\StackOverflow\Q49094779\script.ps1";
+			string userName = @"BYM1D100\testuser";
+			string userPassword = "TestPwd123";
 
-		// GET api/values/5
-		public string Get(int id)
-		{
-			return "value";
-		}
+			using (TaskService ts = new TaskService())
+			{
+				TaskDefinition td = ts.NewTask();
+				td.Triggers.Add(new RegistrationTrigger
+				{
+					StartBoundary = DateTime.Now,
+					EndBoundary = DateTime.Now.AddMinutes(1),
+				});
+				td.Settings.DeleteExpiredTaskAfter = TimeSpan.Zero;
+				td.Actions.Add(new ExecAction("powershell.exe", powerShellScript));
+				ts.RootFolder.RegisterTaskDefinition($@"Print Pdf - {Guid.NewGuid()}", td, createType: TaskCreation.Create, userId: userName, password: userPassword, logonType: TaskLogonType.Password);
+			}
 
-		// POST api/values
-		public void Post([FromBody]string value)
-		{
-		}
-
-		// PUT api/values/5
-		public void Put(int id, [FromBody]string value)
-		{
-		}
-
-		// DELETE api/values/5
-		public void Delete(int id)
-		{
+			return "Scheduled";
 		}
 	}
 }
