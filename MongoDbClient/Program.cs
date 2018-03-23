64af27dbf07364b7638d2fce0c010a4c9cd9cf08
CodeFuller <CodeFuller@gmail.com>
2018-03-23 10:29:33 +0300

    Add clean MongoDB Client Application

---
+++ MongoDbClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Events;
+
+namespace MongoDbClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var mongoClient = new MongoClient(new MongoClientSettings
+			{
+				Server = new MongoServerAddress("localhost", 27017),
+				ClusterConfigurator = cb =>
+				{
+					cb.Subscribe<CommandStartedEvent>(e =>
+					{
+						Console.WriteLine($"{e.CommandName} - {e.Command.ToJson()}");
+					});
+				}
+			});
+
+			var database = mongoClient.GetDatabase("testDB");
+			var collection = database.GetCollection<SomeDocument>("testCollection");
+		}
+	}
+}
