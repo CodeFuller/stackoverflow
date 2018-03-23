f7ef347584a0c6052d7e3c2bbe4bfe1ec51fd71a
CodeFuller <CodeFuller@gmail.com>
2018-03-23 10:58:24 +0300

    Add Unit Test project based on NUnit

---
+++ UnitTests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase("ABC", "abc")]
+		public void TestMethod(string inputData, string expectedResult)
+		{
+			var result = inputData.ToLower(CultureInfo.InvariantCulture);
+
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
