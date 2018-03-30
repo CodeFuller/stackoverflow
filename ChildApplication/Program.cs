a785c95ec0a70ed57f955421b1e1ef15afadaeb7
CodeFuller <CodeFuller@gmail.com>
2018-03-30 20:53:11 +0300

    Q49545611: Child process of .net core application is using parent's configuration file

---
+++ ChildApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ChildApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var configurationBuilder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory());
+			configurationBuilder.AddJsonFile("AppSettings.json", false);
+			var configuration = configurationBuilder.Build();
+			var settingValue = configuration.GetValue<string>("AppName");
+
+			File.WriteAllText(@"d:\ChildAppOutput.txt", settingValue);
+		}
+	}
+}
--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace ConsoleApplication
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConsoleApplication
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var childDllPath = @"d:\Dropbox\prog\StackOverflow\ChildApplication\bin\Debug\netcoreapp2.0\win-x64\publish\ChildApplication.dll";
+			var startInfo = new ProcessStartInfo("dotnet", childDllPath + $" -dt {DateTime.Now.Date.ToString("yyyy-MM-dd")}")
+			{
+				WorkingDirectory = Path.GetDirectoryName(childDllPath),
+			};
+
+			Process.Start(startInfo);
 		}
 	}
 }
