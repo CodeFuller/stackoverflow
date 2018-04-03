0e605cc73641ddb706ecf796894c9964f260d10e
CodeFuller <CodeFuller@gmail.com>
2018-04-03 17:34:48 +0300

    Q49628594: How to Encrypt JwtSecurityToken

--- WebApiApplication/Controllers/ValuesController.cs
+++ WebApiApplication/Controllers/ValuesController.cs
@@ -1,5 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebApiApplication.Controllers
 {
@@ -10,6 +15,62 @@ namespace WebApiApplication.Controllers
 		[HttpGet]
 		public IEnumerable<string> Get()
 		{
+			var user = new
+			{
+				Id = "123",
+				UserName = "Some User",
+			};
+
+			var _appSettings = new
+			{
+				Token = new
+				{
+					Key = "Some long key goes here",
+					Issuer = "Some Issuer",
+					DownloadTokenExpireMin = 1000,
+				}
+			};
+
+			var claims = new[]
+			{
+				new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+				new Claim(JwtRegisteredClaimNames.NameId, user.Id),
+				new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+			};
+
+			string tokenSerialized;
+
+			{
+				var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.Token.Key));
+				var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+				var token = new JwtSecurityToken(_appSettings.Token.Issuer,
+					_appSettings.Token.Issuer,
+					claims,
+					expires: DateTime.Now.AddMinutes(_appSettings.Token.DownloadTokenExpireMin),
+					signingCredentials: creds);
+
+				var tokenHandler = new JwtSecurityTokenHandler();
+				tokenSerialized = tokenHandler.WriteToken(token);
+			}
+
+			{
+				var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.Token.Key));
+
+				TokenValidationParameters validationParameters =
+					new TokenValidationParameters
+					{
+						ValidIssuer = _appSettings.Token.Issuer,
+						ValidAudiences = new[] { _appSettings.Token.Issuer },
+						IssuerSigningKeys = new[] { key }
+					};
+
+				// Now validate the token. If the token is not valid for any reason, an exception will be thrown by the method
+				SecurityToken validatedToken;
+				JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+				var userFromToken = handler.ValidateToken(tokenSerialized, validationParameters, out validatedToken);
+			}
+
 			return new string[] { "value1", "value2" };
 		}
 
