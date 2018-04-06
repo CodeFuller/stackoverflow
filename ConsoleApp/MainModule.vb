43725ce9d5b833ac2d30964eb01f225493deb9cd
CodeFuller <CodeFuller@gmail.com>
2018-04-06 22:14:55 +0300

    Q49477892: ASP.NET: Sign URL with public and private keys from files

---
+++ ConsoleApp/MainModule.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Security.Cryptography
+Imports System.Security.Cryptography.X509Certificates
+
+Module MainModule
+
+	Sub Main()
+
+		Dim signatureUrl = "Test Url"
+		Dim data() As Byte = System.Text.Encoding.ASCII.GetBytes(signatureUrl)
+
+		Dim certificate = New X509Certificate2("d:\temp\keys\keys.pfx", "12345")
+		Dim rsa As RSA = certificate.GetRSAPrivateKey()
+		Dim sig() As Byte = rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1)
+
+	End Sub
+
+End Module
--- ConsoleApplication/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleApplication
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
-}
