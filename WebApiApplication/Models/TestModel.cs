d2fd02a7f6a918e012987b6396867cd0c6e38de2
CodeFuller <CodeFuller@gmail.com>
2018-04-07 14:10:02 +0300

    Q49691685: EnumDataType() attribute validation error message not displaying

---
+++ WebApiApplication/Models/TestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiApplication.Models
+{
+	public class TestModel
+	{
+		[Required]
+		public int? Id { get; set; }
+
+		[EnumDataType(typeof(MyEnum), ErrorMessage = "Custom Error Message")]
+		public MyEnum? MyEnumProp { get; set; }
+	}
+}
---
+++ WebApiApplication/MyEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiApplication
+{
+	public enum MyEnum
+	{
+		None,
+		Value1,
+		Value2
+	}
+}
