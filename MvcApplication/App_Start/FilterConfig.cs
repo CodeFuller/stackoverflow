1706d48f5f709477ad9f29dc96e736cf559566fa
CodeFuller <CodeFuller@gmail.com>
2018-04-07 16:39:00 +0300

    Q49687239: MVC.NET Get full view path in ActionFilterAttribute

--- MvcApplication/App_Start/FilterConfig.cs
+++ MvcApplication/App_Start/FilterConfig.cs
@@ -7,6 +7,8 @@ namespace MvcApplication
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
 			filters.Add(new HandleErrorAttribute());
+
+			filters.Add(new CustomActionFilter());
 		}
 	}
 }
---
+++ MvcApplication/CustomActionFilter.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace MvcApplication
+{
+	public class CustomActionFilter : ActionFilterAttribute
+	{
+		public override void OnResultExecuted(ResultExecutedContext filterContext)
+		{
+			ViewResult viewResult = filterContext.Result as ViewResult;
+			RazorView view = viewResult?.View as RazorView;
+			string viewPath = view?.ViewPath;
+
+			// ...
+		}
+	}
+}
