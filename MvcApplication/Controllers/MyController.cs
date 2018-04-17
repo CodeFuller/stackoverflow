2195306d5e714cafdc43bdf9173d6fb54d3e3283
CodeFuller <CodeFuller@gmail.com>
2018-04-17 08:35:02 +0300

    Q49855194: Context.Request.Query["id"] return null in View

---
+++ MvcApplication/Controllers/MyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApplication.Controllers
+{
+	public class MyController : Controller
+	{
+		public IActionResult Getuser(Guid id)
+		{
+			var path = HttpContext.Request.Path;
+			var id2 = Guid.Parse(path.Value.Split('/').Last());
+
+			return View();
+		}
+	}
+}
